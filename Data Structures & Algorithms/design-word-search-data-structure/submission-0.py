157dca3d085ff17a507d7c131d942556016df70a
Allyssa <allyssa.chiu7@gmail.com>
2026-09-01 03:29:04 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Node()
+
+            curr = curr.children[c]
+
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+
+        def dfs(idx, root):
+
+            curr = root
+
+            for i in range(idx, len(word)):
+                c = word[i]
+
+                if c == '.':
+                    for node in curr.children.values():
+                        if dfs(i + 1, node):
+                            return True
+
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+
+            return curr.endOfWord
+
+
+        return dfs(0, self.root)
+
+        
+        
